ddacaabbfa6556ef15884af4fbf67a8bad9c22b8
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2016-02-23 22:25:10 +0200

    Test pip setup

---
+++ redneckscript/__init__.py
@@ -0,0 +1,2 @@
+def hello_world():
+    return "Hell yeah, world!"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='redneckscript',
+      version='0.1',
+      description='The esiest way to write reusable scripts',
+      url='https://github.com/mrjbee/redneckscript',
+      author='mrjbee',
+      author_email='misterjbee@gmail.com',
+      license='MIT',
+      packages=['redneckscript'],
+      zip_safe=False)
+
